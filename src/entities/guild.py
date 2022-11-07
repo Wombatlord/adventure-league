e95106e4cd8124054955ef98a5eeff8ae0009593
Wombatlord <jordan.stone0@gmail.com>
2022-11-07 22:21:53 +0000

    Random name for guild. Roster size is limited. Check funds and roster limit when recruiting an entity.

--- src/entities/guild.py
+++ src/entities/guild.py
@@ -1,4 +1,13 @@
 from src.entities.entity import Entity
+from random import randint
+
+names = [
+    "Band of the Hawk",
+    "Diamond Dogs",
+    "Screaming Eagles",
+    "Order of the Lion",
+    "Iron Bears",
+]
 
 
 class Guild:
@@ -7,18 +16,39 @@ class Guild:
         name: str = None,
         level: int = None,
         funds: int = None,
+        roster_limit: int = None,
         roster: list[Entity] = None,
     ) -> None:
-        self.name = name
         self.level = level
         self.funds = funds
+        self.roster_scalar = 1.5
+        self.roster_limit = roster_limit
         self.roster = roster
+        self.names = [
+            "Band of the Hawk",
+            "Diamond Dogs",
+            "Screaming Eagles",
+            "Order of the Lion",
+            "Iron Bears",
+        ]
+        self.name = name
+
+        if self.roster_limit == None:
+            self.roster_limit = int(self.level * self.roster_scalar)
+
+        if self.name == None:
+            self.name = self.names.pop(randint(0, len(names) - 1))
 
     def recruit(self, i, pool) -> list[Entity]:
-        if pool[i].cost <= self.funds:
+        if len(self.roster) >= self.roster_limit:
+            print("Roster full.")
+            return
+
+        if pool[i].cost > self.funds:
+            print(f"{pool[i].name.capitalize()} is too expensive.")
+            return
+
+        else:
             self.funds -= pool[i].cost
             choice = pool.pop(i)
             return self.roster.append(choice)
-
-        else:
-            print("Too expensive.")
