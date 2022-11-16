4e48df39614b047410ea2907585d0bcf8dabfbf3
Wombatlord <jordan.stone0@gmail.com>
2022-11-16 21:37:16 +0000

    Separated Monster & Mercenary factory functions.

--- src/entities/dungeon_factory.py
+++ src/entities/dungeon_factory.py
@@ -1,11 +1,11 @@
 from src.entities.dungeon import Dungeon
-from src.entities.fighter_factory import create_random_fighter, create_random_boss
+from src.entities.monster_factory import create_random_monster, create_random_boss
 
 
 def create_random_dungeon(enemy_amount, dungeon_id) -> Dungeon:
     enemies = []
     for _ in range(enemy_amount):
-        enemies.append(create_random_fighter("goblin"))
+        enemies.append(create_random_monster("goblin"))
 
     boss = create_random_boss()
 
