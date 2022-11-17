aba2886bddc523613f3196a05ae4fc2a0148c107
Wombatlord <jordan.stone0@gmail.com>
2022-11-17 05:12:40 +0000

    Moved names & descriptors to constants file.

---
+++ src/config/constants.py
@@ -0,0 +1,63 @@
+# MERCENARY NAMES
+merc_names = [
+    "guts",
+    "raphael",
+    "eliza",
+    "vasquez",
+    "seraph",
+    "emilia",
+    "roberto",
+    "william",
+    "beckett",
+    "freya",
+]
+
+# BOSS NAMES & TITLES
+boss_names = [
+    "griffith",
+    "grux",
+    "kerax",
+    "vestral",
+    "xevrain",
+    "selin",
+    "kefka",
+    "tristan",
+]
+
+boss_titles = [
+    "the Terrible",
+    "the Wicked",
+    "the Dark Knight",
+    "the Cleansing Flame",
+    "the Crimson Promise",
+    "the Broken Dreamer",
+    "the Shattered Echo",
+]
+
+# DUNGEON DESCRIPTION PIECES
+dungeon_descriptors = {
+    "a": [
+        "Forgotten",
+        "Haunted",
+        "Forlorn",
+        "Desolate",
+        "Scourged",
+        "Cursed",
+        "Wailing",
+        "Forsaken",
+        "Scorched",
+        "Frozen",
+    ],
+    "b": [
+        "Crypt",
+        "Church",
+        "Cathedral",
+        "Sepulcher",
+        "Warcamp",
+        "Cave",
+        "Tunnels",
+        "Mine",
+        "Forest",
+        "Lair",
+    ],
+}
