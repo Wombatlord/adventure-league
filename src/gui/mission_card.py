dc1e7d51c04896706920a49e58cf61143bf99c84
Wombatlord <jordan.stone0@gmail.com>
2022-11-26 05:21:51 +0000

    MissionCard object for drawing mission cards and populating with details from the engine.

---
+++ src/gui/mission_card.py
@@ -0,0 +1,48 @@
+import arcade
+from src.engine.engine import eng
+
+class MissionCard:
+    def __init__(self, width, height, mission, margin):
+        self.width = width
+        self.height = height
+        self.mission = mission
+        self.margin = margin
+
+    def draw_card(self):
+
+        y = (
+                (self.margin + self.height) * self.mission
+                + self.margin
+                + self.height // 2
+            )
+
+        arcade.draw_rectangle_outline(
+                center_x=self.width * 0.5,
+                center_y=y / 3,
+                width=self.width - self.margin,
+                height=self.height * 0.3,
+                color=arcade.color.GOLDENROD,
+            )
+
+        arcade.Text(
+                text=eng.mission_board.missions[self.mission].description,
+                start_x=self.margin * 5,
+                start_y=y / 3 + self.height * 0.11,
+                font_name="Alagard",
+            ).draw()
+
+        arcade.Text(
+                text="Boss: ",
+                start_x=self.margin * 9,
+                start_y=y / 3 + self.height * 0.05,
+                font_name="Alagard",
+                color=arcade.color.GOLDENROD
+            ).draw()
+
+        arcade.Text(
+                text=f"{eng.mission_board.missions[self.mission].boss.name.name_and_title()}",
+                start_x=self.margin * 20,
+                start_y=y / 3 + self.height * 0.05,
+                font_name="Alagard",
+                color=arcade.color.RED
+            ).draw()
