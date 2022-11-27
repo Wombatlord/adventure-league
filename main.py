7c8df2fee3b7d886beb945eeeb23332d70637995
Wombatlord <jordan.stone0@gmail.com>
2022-11-27 19:12:07 +0000

    Moved startup func implementation to main.py from src/gui/views.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.gui.views import start_adventure_league
+import arcade
+from src.gui.views import WindowData, TitleView
+
+def start_adventure_league():
+    """Startup"""
+    window = arcade.Window(
+        WindowData.width, WindowData.height, "Adventure League!", resizable=True
+    )
+    title_view = TitleView(window=window)
+    window.show_view(title_view)
+    arcade.run()
 
 start_adventure_league()
