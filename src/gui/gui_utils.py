c30cb7cb46e879d5950e5952056d6ad9a09722d1
Wombatlord <jordan.stone0@gmail.com>
2022-11-28 03:18:15 +0000

    Focus selection via Cycle class. Sketched Grid position selection.

---
+++ src/gui/gui_utils.py
@@ -0,0 +1,43 @@
+from typing import NamedTuple
+
+class Cycle:
+    def __init__(self, length: int, pos: int = 0) -> None:
+        self.length = length
+        self.pos = pos
+
+    def incr(self):
+        self.pos = (self.pos + 1) % self.length
+
+    def decr(self):
+        self.pos = (self.pos - 1) % self.length
+
+
+class Coords(NamedTuple):
+    x: int
+    y: int
+
+class Grid:
+    axes: tuple[Cycle, Cycle]
+    def __init__(
+        self, 
+        size: tuple[int, int], # (width, height) 
+        pos: tuple[int,int] = (0,0), # (x, y)
+    ) -> None:
+        self.axes = tuple(
+            Cycle(length, start) for length, start in zip(size, pos)
+        )
+
+    def current(self) -> Coords:
+        return Coords(self.axes[0].pos, self.axes[1].pos)
+
+    def left(self):
+        self.axes[0].decr()
+
+    def right(self):
+        self.axes[0].incr()
+
+    def up(self):
+        self.axes[1].incr()
+
+    def down(self):
+        self.axes[1].decr()
