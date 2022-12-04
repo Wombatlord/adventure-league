f552cb8d0b2e320de6605a4878a64013f314f631
Wombatlord <jordan.stone0@gmail.com>
2022-12-04 05:56:17 +0000

    Better test coverage for ScrollWindow behaviour

--- src/tests/test_scroll_window.py
+++ src/tests/test_scroll_window.py
@@ -58,3 +58,88 @@ class ScrollWindowTest(unittest.TestCase):
         assert (
             actual == expected
         ), f"The frame was not dragged as expected: {expected=}, {actual=}"
+
+    def test_selection_is_valid_when_selection_is_max_and_pop_is_called(self):
+        # Arrange
+        sc = ScrollWindow(self.strings, 3)
+        
+        sc.incr_selection()
+        sc.incr_selection()
+        sc.incr_selection()
+        sc.incr_selection()
+        sc.incr_selection()
+
+        # Action
+        sc.pop(-1)
+        
+        # Assert
+        assert(
+            sc.position.max == len(self.strings) - 2
+        ), f"The position cycle length was not adjusted: {sc.position.max=} {len(sc.items)=}"
+
+        assert(
+            sc.position.pos == sc.position.max
+        ), f"The position was not dragged by the pop: {sc.position.pos=}, {sc.position.max=}"
+
+        assert(
+            sc.frame_end == sc.position.max + 1
+        ), f"The frame was not dragged to the selection: {sc.frame_end=} {sc.position.max=}"
+    
+    def test_selection_is_valid_when_selection_is_zero_and_pop_is_called_with_zero(self):
+        # Arrange
+        sc = ScrollWindow(self.strings, 3)
+
+        # Action
+        sc.pop(0)
+
+        # Assert
+        assert(
+            sc.position.pos == 0
+        ), f"The position should remain at zero: {sc.position.pos=}"
+
+        assert(
+            sc.frame_start == 0
+        ), f"The frame_start should remain at zero: {sc.frame_start=}"
+
+    def test_selection_hoists_tail_when_middle_element_is_popped(self):
+        # Arrange
+        sc = ScrollWindow(self.strings, 3)
+        
+        # Action
+        sc.incr_selection() # selection down one from top
+        
+        sc.pop(sc.position.pos) # pop selection
+
+        # Assert
+        expected = (
+            [
+                *self.strings[:1], # one element before the popped selection
+                *self.strings[2:4], # two elements after the popped selection
+            ], 
+            1, # The middle element is selected
+        )
+        assert (
+            sc.visible_items == expected
+        ), f"The visible items and selection should be {expected=}, we got {sc.visible_items=}"
+
+    def test_selection_hoists_tail_when_penultimate_element_is_popped(self):
+        # Arrange
+        sc = ScrollWindow(self.strings, 3)
+        
+        # Action
+        for _ in range(5):  # selection down five from top
+            sc.incr_selection()
+        sc.decr_selection() # then back up one from bottom
+
+        sc.pop(sc.position.pos) # pop selection
+
+        # Assert
+        expected = (
+            [
+                *self.strings[-4:-2], # the two elements before the popped item
+                *self.strings[-1:]], # the final element
+            2, # if the tail is hoisted, our selection is now the final item
+        )
+        assert (
+            sc.visible_items == expected
+        ), f"The visible items and selection should be {expected=}, we got {sc.visible_items=}"
