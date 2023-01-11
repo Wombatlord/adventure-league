500449324a9967097bd1c793d01efeb49fa22e72
Wombatlord <jordan.stone0@gmail.com>
2023-01-11 21:45:06 +0000

    cross-platform check for font names

--- src/gui/window_data.py
+++ src/gui/window_data.py
@@ -1,5 +1,15 @@
 import arcade
+import pyglet
 from dataclasses import dataclass
+from pyglet.libs.win32.constants import WINDOWS_7_OR_GREATER
+
+pyglet.font.add_file("./assets/alagard.ttf")
+
+def _cross_platform_name(name: str) -> str:
+    if pyglet.compat_platform not in (WINDOWS_7_OR_GREATER, "darwin"):
+        return name.lower()
+
+    return name
 
 @dataclass
 class WindowData:
@@ -7,4 +17,4 @@ class WindowData:
     height = 600
     title_background = arcade.load_texture("./assets/background_glacial_mountains.png")
     mission_background = arcade.load_texture("./assets/mb.png")
-    font = "Alagard"
+    font = _cross_platform_name("Alagard")
