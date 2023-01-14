02579d242584b48a18efcc602fe77855a2da0807
Wombatlord <jordan.stone0@gmail.com>
2023-01-14 19:24:07 +0000

    factory methods for creating a dungeon with rooms

--- src/entities/dungeon_factory.py
+++ src/entities/dungeon_factory.py
@@ -13,12 +13,15 @@ def describe_dungeon() -> str:
     ]
     return f"The {descriptor_a} {descriptor_b}"
 
+
 def create_random_dungeon(enemy_amount) -> Dungeon:
     enemies = []
     for i in range(enemy_amount):
         enemies.append(create_random_monster(f"goblin {i}", None))
 
     return Dungeon(
+        0,
+        [],
         enemies,
         create_random_boss(
             name=boss_names[randint(0, len(boss_names) - 1)],
@@ -35,21 +38,19 @@ def create_random_enemy_room(enemy_amount):
     room = Room()
 
     for enemy in range(enemy_amount):
-        room.enemies.append(create_random_monster(f"goblin {enemy}", None))
-    
-    return room
+        room.add_entity(create_random_monster(f"goblin {enemy}", None))
 
+    return room
 
 def create_random_boss_room():
     room = Room()
 
-    room.enemies.append(
+    room.add_entity(
         create_random_boss(
             name=boss_names[randint(0, len(boss_names) - 1)],
             title=boss_titles[randint(0, len(boss_titles) - 1)],
         )
     )
-
     return room
 
 
@@ -62,14 +63,20 @@ def create_dungeon_with_boss_room(room_amount: int):
             name=boss_names[randint(0, len(boss_names) - 1)],
             title=boss_titles[randint(0, len(boss_titles) - 1)],
         ),
+        describe_dungeon(),
+        treasure=randint(100, 150),
+        xp_reward=10,
     )
     for _ in range(room_amount):
         e = randint(1, d.max_enemies_per_room)
+        print(e)
         d.rooms.append(create_random_enemy_room(enemy_amount=e))
-    
+
     d.rooms.append(create_random_boss_room())
 
-    print(f"{d.rooms=}")
+    return d
 
-    for i, _ in enumerate(d.rooms):
-        print(f"{d.rooms[i].enemies=}")
+    # print(f"{d.rooms=}")
+
+    # for i, _ in enumerate(d.rooms):
+    #     print(f"{d.rooms[i].enemies=}")
