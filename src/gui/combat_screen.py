940726ad29f13a7f36bdcba6abe64fa0d8ec525d
Wombatlord <jordan.stone0@gmail.com>
2023-01-16 08:07:45 +0000

    heights for message display with gui_utils.gen_heights()

--- src/gui/combat_screen.py
+++ src/gui/combat_screen.py
@@ -3,26 +3,40 @@ from src.engine.engine import eng
 from src.gui.window_data import WindowData
 from src.gui.gui_utils import gen_heights
 
+
 class CombatScreen:
     def __init__(self) -> None:
         self.messages = eng.messages
         self.message = "None"
         self.display_messages = []
-        self.heights = [200,240,280,320,360]
+        self.heights = []
+
     def next_message(self):
+        if len(self.heights) == 0:
+            heights = gen_heights(
+                desc=False,
+                row_height=40,
+                y=WindowData.height / 2,
+                spacing=2,
+                max_height=WindowData.height,
+            )
+            for height in heights:
+                print(height)
+                self.heights.append(height)
+
         if len(self.messages) > 0:
             self.message = self.messages.pop(0)
             self.display_messages.append(self.message)
-            
+
             if len(self.display_messages) > 5:
                 self.display_messages.pop(0)
-    
+
     def draw_message(self):
-        #message = self.message
-        
+        # message = self.message
+
         if len(self.display_messages) > 0:
             for i, message in enumerate(self.display_messages):
-            
+
                 arcade.Text(
                     text=message,
                     start_x=WindowData.width / 2,
