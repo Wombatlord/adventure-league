286059b7834b72d3c7f84381f3a28b6383537ca1
Wombatlord <jordan.stone0@gmail.com>
2023-01-17 08:00:43 +0000

    Coloured text and decreasing alpha values for messages. MessageWithID named tuple.

--- src/gui/combat_screen.py
+++ src/gui/combat_screen.py
@@ -1,15 +1,22 @@
 import arcade
+from typing import NamedTuple
 from src.engine.engine import eng
 from src.gui.window_data import WindowData
 from src.gui.gui_utils import gen_heights
 
+class MessageWithID(NamedTuple):
+    message: str
+    id: int
 
 class CombatScreen:
     def __init__(self) -> None:
         self.messages = eng.messages
-        self.message = "None"
-        self.display_messages = []
+        self.latest_message = MessageWithID("No Message", 0)
+        self.display_messages: list[MessageWithID] = []
         self.heights = []
+        self.alphas = []
+        self.alpha_max = 255
+        self.message_id = 0
 
     def next_message(self):
         if len(self.heights) == 0:
@@ -25,20 +32,28 @@ class CombatScreen:
                 self.heights.append(height)
 
         if len(self.messages) > 0:
-            self.message = self.messages.pop(0)
-            self.display_messages.append(self.message)
-
+            self.latest_message = MessageWithID(self.messages.pop(0), self.message_id)
+            self.display_messages.append(self.latest_message)
+            self.message_id += 1
             if len(self.display_messages) > 5:
                 self.display_messages.pop(0)
 
+        if len(self.alphas) < 4:
+            self.alphas.append(self.alpha_max)
+            self.alpha_max -= 50
+            self.alphas.sort()
+            print(self.alphas)
+    
     def draw_message(self):
-        # message = self.message
-
         if len(self.display_messages) > 0:
-            for i, message in enumerate(self.display_messages):
-
+            for i, current_message in enumerate(self.display_messages):
+                
+                if current_message.message == self.latest_message.message and current_message.id == self.latest_message.id:
+                    color = (218, 165, 32, 255)
+                else:
+                    color = (255,255,255, self.alphas[i])
                 arcade.Text(
-                    text=message,
+                    text=current_message.message,
                     start_x=WindowData.width / 2,
                     start_y=self.heights[i],
                     anchor_x="center",
@@ -46,5 +61,6 @@ class CombatScreen:
                     multiline=True,
                     width=500,
                     align="center",
+                    color=color,
                     font_name=WindowData.font,
                 ).draw()
