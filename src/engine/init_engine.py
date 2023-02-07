2b2f481dabd461b01f891046363d505a2a6bfe9e
Wombatlord <jordan.stone0@gmail.com>
2023-02-07 01:24:29 +0000

    engine bootstrap function to ensure singleton remains a singleton

--- src/engine/init_engine.py
+++ src/engine/init_engine.py
@@ -1,11 +1,25 @@
 from src.engine.engine import Engine
-from src.engine.describer import Describer
 
 # Instantiate & setup the engine
-eng = Engine(describer=Describer())
-eng.setup()
 
-# Get some entities in the guild
-eng.recruit_entity_to_guild(0)
-eng.recruit_entity_to_guild(0)
-eng.recruit_entity_to_guild(0)
+_eng: Engine | None = None
+
+def _bootstrap():
+    global _eng    
+    _eng = Engine()
+    _eng.setup()
+
+    # Get some entities in the guild
+    _eng.recruit_entity_to_guild(0)
+    _eng.recruit_entity_to_guild(0)
+    _eng.recruit_entity_to_guild(0)
+    
+def _get_eng() -> Engine:
+    global _eng
+    if _eng is not None:
+        return _eng
+    
+    _bootstrap()
+    return _eng
+
+eng = _get_eng()
