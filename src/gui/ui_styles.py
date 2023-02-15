592ce8baac8d609288f829795e7348c779031814
Wombatlord <jordan.stone0@gmail.com>
2023-02-15 07:45:14 +0000

    extracted button styling to own module

---
+++ src/gui/ui_styles.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from typing import Optional
+import arcade
+from arcade import Color
+from arcade.text import FontNameOrNames
+from arcade.gui import UIStyleBase
+from src.gui.window_data import WindowData
+
+@dataclass
+class UIStyle(UIStyleBase):
+    font_size: int = 12
+    font_name: FontNameOrNames = WindowData.font
+    font_color: Color = arcade.color.WHITE
+    bg: Color = arcade.color.BLACK
+    border: Optional[Color] = None
+    border_width: int = 0
+
+ADVENTURE_STYLE = {
+        "normal": UIStyle(),
+        "hover": UIStyle(
+            font_size=12,
+            font_name=WindowData.font,
+            font_color=arcade.color.GOLD,
+            bg=(21, 19, 21),
+            border=None,
+            border_width=0,
+        ),
+        "press": UIStyle(
+            font_size=12,
+            font_name=WindowData.font,
+            font_color=arcade.color.BLACK,
+            bg=arcade.color.WHITE,
+            border=None,
+            border_width=0,
+        ),
+        "disabled": UIStyle(
+            font_size=12,
+            font_name=WindowData.font,
+            font_color=arcade.color.WHITE,
+            bg=arcade.color.GRAY,
+            border=None,
+            border_width=0,
+        )
+    }
