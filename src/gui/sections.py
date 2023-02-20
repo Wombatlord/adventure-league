f48b774ce6dfb907a1ef3d0e2ed567075209f052
Wombatlord <jordan.stone0@gmail.com>
2023-02-20 00:36:57 +0000

    Implemented InfoPanelSection for displaying some general info to user depding on configuration in a View

--- src/gui/sections.py
+++ src/gui/sections.py
@@ -1,6 +1,7 @@
 import arcade
 from arcade.gui import UIManager
 from arcade.gui.widgets.buttons import UIFlatButton
+from arcade.gui.widgets.text import UILabel, UITextArea
 from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout
 
 from src.gui.buttons import CommandBarMixin
@@ -12,7 +13,7 @@ class CommandBarSection(arcade.Section, CommandBarMixin):
     anchor: UIAnchorLayout
     command_box: UIBoxLayout
     buttons: list[UIFlatButton]
-    
+
     def __init__(
         self,
         left: int,
@@ -20,7 +21,7 @@ class CommandBarSection(arcade.Section, CommandBarMixin):
         width: int,
         height: int,
         buttons: list[UIFlatButton],
-        **kwargs
+        **kwargs,
     ):
         super().__init__(left, bottom, width, height, **kwargs)
 
@@ -37,54 +38,108 @@ class CommandBarSection(arcade.Section, CommandBarMixin):
         Typically in a View's on_show_view, but can also be in response to a UIEvent if that event changes the available buttons.
         """
         self.compose_command_bar(self.manager, self.command_bar)
-    
+
     def on_draw(self) -> None:
         self.manager.draw()
 
-    def compose_command_bar(self, manager: UIManager, buttons: list[UIFlatButton]) -> None:
+    def compose_command_bar(
+        self, manager: UIManager, buttons: list[UIFlatButton]
+    ) -> None:
         """
         Attaches the root node of the layout to the provided manager.
         Buttons are attached to the UIBoxLayout, which is the child of the root node.
-        
+
         Args:
             manager (UIManager): The UIManager of a View which wants to have a command bar.
             buttons (list[UIFlatButton]): An array of UIFlatButtons with their own handlers already attached.
         """
         anchor = manager.add(UIAnchorLayout())
-        command_box = (
-            UIBoxLayout(
-                vertical=False,
-                height = self.height,
-                children=buttons,
-                size_hint=(1, None),
-            )
-            .with_padding()
-        )
+        command_box = UIBoxLayout(
+            vertical=False,
+            height=self.height,
+            children=buttons,
+            size_hint=(1, None),
+        ).with_padding()
 
         anchor.add(
             anchor_x="center_x",
             anchor_y="bottom",
             child=command_box,
         )
-        
+
     def style_command_bar(self, buttons: list[UIFlatButton]) -> list[UIFlatButton]:
         """
         style_command_bar expects to be passed a list of buttons that will occupy
         the command bar UI component. It applies consistent styling and spacing to the buttons
         in place so that they have responsive scaling.
-        
+
         Args:
             buttons (list[UIFlatButton]): the list of buttons to be styled.
 
         Returns:
             list[UIFlatButton]: the styled (in place) buttons.
-        """    
+        """
         if not buttons:
             return []
 
         for button in buttons:
-            button.size_hint=(1/len(buttons), 1)
-            button.style=ADVENTURE_STYLE
+            button.size_hint = (1 / len(buttons), 1)
+            button.style = ADVENTURE_STYLE
             button.with_border(width=2, color=arcade.color.GOLDENROD)
 
         return buttons
+
+
+class InfoPaneSection(arcade.Section):
+    def __init__(
+        self,
+        left: int,
+        bottom: int,
+        width: int,
+        height: int,
+        margin: int,
+        texts: list[UILabel],
+        **kwargs,
+    ):
+        super().__init__(left, bottom, width, height, **kwargs)
+
+        self.manager = UIManager()
+        self.margin = margin
+        self.texts = texts
+    
+    def setup(self) -> None:
+        self.compose_info_bar()
+    
+    def compose_info_bar(
+        self
+    ) -> None:
+        """
+        Attaches the root node of the layout to the provided manager.
+        Buttons are attached to the UIBoxLayout, which is the child of the root node.
+
+        Args:
+            manager (UIManager): The UIManager of a View which wants to have a command bar.
+            buttons (list[UIFlatButton]): An array of UIFlatButtons with their own handlers already attached.
+        """
+        anchor = self.manager.add(UIAnchorLayout())
+        command_box = UIBoxLayout(
+            vertical=True,
+            height=self.height,
+            size_hint=(1, None),
+            align="top",
+            children=self.texts,
+        ).with_padding(top=10).with_border(color=arcade.color.RED_DEVIL)
+
+        anchor.add(
+            anchor_x="center_x",
+            anchor_y="bottom",
+            align_y=self.bottom,
+            child=command_box,
+        )
+
+    def on_draw(self):
+        self.manager.draw()
+
+    def on_resize(self, width: int, height: int):
+        super().on_resize(width, height)
+        self.width = width
