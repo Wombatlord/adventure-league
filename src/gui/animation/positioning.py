382499f377943b86cf015df368e2ee3b7c8b2f59
Wombatlord <jordan.stone0@gmail.com>
2023-04-14 21:25:24 +0100

    more sensible maintain_position impl

--- src/gui/animation/positioning.py
+++ src/gui/animation/positioning.py
@@ -14,20 +14,13 @@ def maintain_position(v1: Vec2 | tuple, v2: Vec2 | tuple, thing) -> Vec2:
     Returns:
         Vec2: The new (x,y) of the thing after the scaling operations.
     """
-
-    if isinstance(v1, tuple):
-        v1 = Vec2(v1[0], v1[1])
-
-    if isinstance(v2, tuple):
-        v2 = Vec2(v2[0], v2[1])
+    before_vec = Vec2(*v1)
+    after_vec = Vec2(*v2)
 
     # Calculate the scale factor of the x and y components.
     # For example, the scale factor of the vector corresponding to a window resize
-    current_vec = Vec2(math.sqrt(v1.x**2), math.sqrt(v1.y**2))
-    new_vec = Vec2(math.sqrt(v2.x**2), math.sqrt(v2.y**2))
-
-    scale_factor_x = new_vec.x / current_vec.x
-    scale_factor_y = new_vec.y / current_vec.y
+    scale_factor_x = after_vec.x / before_vec.x
+    scale_factor_y = after_vec.y / before_vec.y
 
     # Set new (x, y) of the thing based on scaled vector.
     # For example, a UIAnchorLayout that should maintain its position relative to the window after scaling the window.
