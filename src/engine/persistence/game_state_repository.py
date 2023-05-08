ee14d9742996a8315287d7a676f22729d2739f4c
Wombatlord <Wombatlord@users.noreply.github.com>
2023-05-08 18:26:53 +0100

    Update src/engine/persistence/game_state_repository.py
    Co-authored-by: tjweldon <38912305+tjweldon@users.noreply.github.com>

--- src/engine/persistence/game_state_repository.py
+++ src/engine/persistence/game_state_repository.py
@@ -7,19 +7,56 @@ from src.engine.guild import Guild
 from src.engine.init_engine import eng
 from src.engine.persistence.loaders import GameStateLoaders
 
+from enum import Enum
+
+class Format(Enum):
+    YAML = "yaml"
+    PICKLE = "pikl"
+    
+    def dumper(self):
+        return {
+            Format.YAML: dump,
+            Format.PICKLE: pickle.dump,
+        }[self]
+        
+    def loader(self):
+        return {
+            Format.YAML: lambda file: load(file, Loader),
+            Format.PICKLE: pickle.load,
+        }[self]
+    
+    def dump(self, data, path):
+        with open(path, "wb+") as save_file:
+            self.dumper(data, save_file)
+            
+     def load(self, path) -> dict:
+         with open(path, "r") as save_file:
+            state = self.loader(save_file)
+            
+         return state
+        
 
 class GameStateRepository:
     @classmethod
-    def save_file_path(cls, slot: int, fmt: str = "pikl") -> tuple[str, ...]:
-        match fmt:
-            case "pikl":
-                return str(SAVE_FILE_DIRECTORY / f"save_{slot}.pikl")
-            case "yaml":
-                return str(SAVE_FILE_DIRECTORY / f"save_{slot}.yaml")
-            case _:
-                raise ValueError(
-                    f"Unsupported format. Got {fmt=}, expected: pikl / yaml"
-                )
+    def save_file_path(cls, slot: int, fmt: Format = Format.PICKLE) -> tuple[str, ...]:
+        if not isinstance(fmt, Format):
+            raise TypeError(f"Unrecognised format {fmt}")
+        return str(SAVE_FILE_DIRECTORY / f"save_{slot}.{fmt.value}")
+    
+    @classmethod
+    def load(cls, slot, fmt=Format.PICKLE):
+        if not isinstance(fmt, Format):
+            raise TypeError(f"Unrecognised format {fmt}")
+            
+        return GameStateLoaders.guild_from_dict(fmt.load(self.save_file_path(slot, fmt)))
+        
+        
+    @classmethod
+    def save(cls, slot, state, fmt=Format.PICKLE):
+        if not isinstance(fmt, Format):
+            raise TypeError(f"Unrecognised format {fmt}")
+            
+        fmt.dump(GameStateDumpers.guild_to_dict(state))
 
     @classmethod
     def save_yaml(cls, slot: int, data: dict):
