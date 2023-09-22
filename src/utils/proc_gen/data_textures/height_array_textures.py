d0bdd54db50541c8ede0960d27a71205bf7e7621
Wombatlord <jordan.stone0@gmail.com>
2023-09-22 23:24:16 +0100

    prototyping texture grid thing

--- src/utils/proc_gen/data_textures/height_array_textures.py
+++ src/utils/proc_gen/data_textures/height_array_textures.py
@@ -5,12 +5,20 @@ from typing import Generator
 def rgba(r: int, g: int, b: int, a: int) -> np.array:
     return np.array([r, g, b, a], dtype=np.uint8)
 
+def grey(g: int):
+    return rgba(g, g, g, g)
+
 
 class DataTexture:
     pixels: np.array
 
     def __init__(self, size: tuple[int, int], dtype=np.uint8):
         self.pixels = np.zeros(size + (4,), dtype=dtype)
+        
+    def __getitem__(self, *indices):
+        if len(indices) >= 2:
+            indices = indices[:2][::-1] + indices[2:]
+        return self.pixels.__getitem__(*indices)
 
     def scanner(self) -> Generator[np.array, None, None]:
         for y in range(self.pixels.shape[1]):
@@ -25,3 +33,11 @@ class DataTexture:
         return buffer
     
 
+height_map = DataTexture((10, 10), dtype=np.uint8)
+ground = grey(8)
+pillar = grey(16)
+height_map.pixels[:, :] = ground
+border=3
+height_map.pixels[border:-border, border:-border] = pillar
+
+print(height_map[:, :, 0])
